0c9d42b00a97b4e5cfbc634b75d3d54e7ab2d413
lone5402 <hackerbest@foxmail.com>
2019-10-21 23:09:10 +0800

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
 
-    public int maxProfit(int[] prices) {
-        if (prices.length == 0)
-            return 0;
-        int min = prices[0];
-        int profit = 0;
-        for (int p : prices) {
-            if (p - min > 0) {
-                profit = Math.max(p - min, profit);
-            } else {
-                min = p;
-            }
-        }
-        return profit;
-    }
-
 
     public static void main(String[] args) {
-        int[] nums = {1, 4, 5, 6, 9};
-        new Main().maxProfit(nums);
-
+        int[] arr = { 3, 2, 3 };
+        System.out.println(new Main().majorityElement(arr));
     }
-
 
 }
