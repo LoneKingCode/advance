6302062eda70de27ef1d890ebbbb5c74909da464
LoneKingCode <hackerbest@foxmail.com>
2021-05-29 20:36:44 +0800

    code401 1

--- src/leetcode/backtracking/EasyCode401.java
+++ src/leetcode/backtracking/EasyCode401.java
@@ -22,27 +22,90 @@ import java.util.List;
  * @Date : 2021/5/26
  */
 public class EasyCode401 {
-    List<String> result = new ArrayList<>();
-    int[] minutesArr = new int[]{1, 2, 4, 8, 16, 32};
-    int[] hourArr = new int[]{1, 2, 4, 8};
 
-    public List<String> readBinaryWatch(int turnedOn) {
-        if (turnedOn > 8 || turnedOn < 1) {
+    List<String> result2 = new ArrayList<>();
+
+    public int bCount(int n) {
+        int count = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
+
+        }
+        return count;
+    }
+
+    public List<String> readBinaryWatch2(int turnedOn) {
+        if (turnedOn < 0) {
             return new ArrayList<>();
         }
+        // 所有组合都列出来 计算二进制数的1的个数
+        for (int i = 0; i < 60; i++) {
+            for (int j = 0; j < 12; j++) {
+                if (bCount(i) + bCount(j) == turnedOn) {
+                    result2.add(j + ":" + (i < 10 ? "0" + i : i));
+                }
+            }
+        }
+        return result2;
+    }
 
+
+    // 回溯
+
+    List<String> result = new ArrayList<>();
+    int[] timeArr = new int[]{1, 2, 4, 8, 1, 2, 4, 8, 16, 32};
+
+    public List<String> readBinaryWatch(int turnedOn) {
+        if (turnedOn < 0) {
+            return new ArrayList<>();
+        }
+        f(turnedOn, 0, 0, 0);
         return result;
     }
 
-    public void f(int turnedOn,int current, int hour, int minutes) {
-        if(current == turnedOn)
-        {
-            // save result
+    public void f(int turnedOn, int timeStartIndex, int hourSum, int minutesSum) {
+        if (turnedOn == 0) {
+            if (hourSum > 11 || minutesSum > 59) {
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(hourSum);
+
+            sb.append(":");
+            if (minutesSum < 10) {
+                sb.append(0);
+            }
+            sb.append(minutesSum);
+
+            result.add(sb.toString());
             return;
+        }
+
+        for (int i = timeStartIndex; i < timeArr.length; i++) {
+            if (i < 4) {
+                hourSum += timeArr[i];
+
+            } else {
+                minutesSum += timeArr[i];
+            }
+            f(turnedOn - 1, i + 1, hourSum, minutesSum);
+
+            if (i < 4) {
+                hourSum -= timeArr[i];
+
+            } else {
+                minutesSum -= timeArr[i];
+
+            }
+
         }
     }
 
     public static void main(String[] args) {
-
+        System.out.println(new EasyCode401().readBinaryWatch2(1));
     }
 }
